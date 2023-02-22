12c9a0d86fe7c1be6bf5581894c39a501a73e4b9
Rebecca Scott <me@becdetat.com>
2023-02-22 14:09:07 +1000

    DELETE ALL THE THINGS!!!1!

--- src/partsbin/Helpers/PresentationFormatExtension.cs
+++
@@ -1,9 +0,0 @@
-namespace partsbin.Helpers;
-
-public static class PresentationFormatExtension
-{
-    public static string? PresentationFormat(this string? value)
-    {
-        return value.HasContent() ? value : "---";
-    }
-}
--- src/partsbin/Services/Results/PartTypeAndRangesResult.cs
+++
@@ -1,8 +0,0 @@
-namespace partsbin.Services.Results;
-
-public class PartTypeAndRangesResult
-{
-    public string? PartType;
-    public string? Range;
-    public int Count;
-}
