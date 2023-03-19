5d68119b41ab164c86335882637c6a0e8f266d48
Rebecca Scott <me@becdetat.com>
2023-03-19 15:07:25 +1000

    Clean up quill toolbar, and disable pasting images and videos

--- src/partsbin/wwwroot/js/BlazorQuill.js
+++ src/partsbin/wwwroot/js/BlazorQuill.js
@@ -1,5 +1,30 @@
 // https://www.puresourcecode.com/dotnet/blazor/create-a-blazor-component-for-quill/
 (function () {
+    // Explicitly disable images and video from being pasted in
+    // (plus formula, which actually could be handy in the future) 
+    var formats = [
+        'background',
+        'bold',
+        'color',
+        'font',
+        'code',
+        'italic',
+        'link',
+        'size',
+        'strike',
+        'script',
+        'underline',
+        'blockquote',
+        'header',
+        'indent',
+        'list',
+        'align',
+        'direction',
+        'code-block',
+        //'formula',
+        //'image',
+        //'video',
+    ];
     window.QuillFunctions = {
         createQuill: function (quillElement, toolBar, readOnly, placeholder, theme, debugLevel) {
             var options = {
@@ -9,7 +34,8 @@
                 },
                 placeholder: placeholder,
                 readOnly: readOnly,
-                theme: theme
+                theme: theme,
+                formats: formats
             };
 
             new Quill(quillElement, options);
